48f217a3946dec9e828f86476fcccefac682c515
Bohdan <bohdanpolishchuk@gmail.com>
2016-09-09 18:02:18 +0300

    Add files via upload

--- send.php
+++ send.php
@@ -10,7 +10,7 @@ if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST[
                     <body>
                         <p>Имя: '.$_POST['name'].'</p>
                         <p>Телефон: '.$_POST['phone'].'</p> 
-                        <p>Почта: '.$_POST['mail'].'</p>                        
+                                                
                     </body>
                 </html>'; //Текст нащего сообщения можно использовать HTML теги
         $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
